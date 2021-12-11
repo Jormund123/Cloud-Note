55cfbe408032122e19e7268c551171bd7918eafb
Jormund123 <anand.karna.social@gmail.com>
2021-12-11 12:58:07 +0545

    Adding jwt authentication, salt & bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require("express-validator");
+var jwt = require('jsonwebtoken');
+
+//jwt authentication is also used to provide a unique token to the user
+const JWT_SECRET ='AnandKarna';
 
 //creating a user using: POST"/api/auth/createuser" (Doesn't require Auth)
 router.post(
@@ -34,13 +38,23 @@ router.post(
           .json({ error: "Sorry a user with this email already exists" });
       }
 
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       //creating a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+
+      //jwt authentication
+      const data = {
+        user: {
+          id: user.id,
+        }
+      }
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({authToken});
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Something went wrong"); //if we do some wrong with the keywords and variables
