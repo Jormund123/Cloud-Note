495c09fe55257ab7bd2711aba2261f84ffc0f462
Jormund123 <anand.karna.social@gmail.com>
2021-12-12 13:26:54 +0545

    MiddleWare creation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
+var fetchuser = require("../middleware/fetchuser");
 
 //jwt authentication is also used to provide a unique token to the user
-const JWT_SECRET ='AnandKarna';
+const JWT_SECRET = "AnandKarna";
 
-//creating a user using: POST"/api/auth/createuser" (Doesn't require Auth)
+//Route 1
+//creating a user using: POST"/api/auth/createuser" (No login required)
 router.post(
   "/createuser",
   [
@@ -51,13 +53,77 @@ router.post(
       const data = {
         user: {
           id: user.id,
-        }
-      }
+        },
+      };
       const authToken = jwt.sign(data, JWT_SECRET);
-      res.json({authToken});
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Something went wrong"); //if we do some wrong with the keywords and variables
+    }
+  }
+);
+
+//next endpoint
+//Route 2
+//authenticate a user using: POST "/api/auth/login". No login required
+router.post(
+  "/login",
+  [
+    body("email", "Invalid Email").isEmail(),
+    body(
+      "password",
+      "The length of the password should be at least 6."
+    ).isLength({ min: 6 }),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    //if there are errors then bad request and errors are returned
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Login credientials not valid" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({ error: "Login credientials not valid" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Something went wrong");
+    }
+  }
+);
+
+//Route 3
+//get logged in user details: POST "/api/auth/getuser". login required
+router.post(
+  "/getuser",
+   fetchuser,
+  async (req, res) => {
+    try {
+      userId = req.user.id;
+      const user = await User.findById(userId).select("-password"); //when getting the details of the user, password will not be shown
+      res.send(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Something went wrong");
     }
   }
 );
